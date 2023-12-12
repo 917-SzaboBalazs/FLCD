3cd9101c8d0d3af154e18f7797ce407013fe93fb
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-12-12 18:14:10 +0200

    lab6 setup

---
+++ lab6/ll1.py
@@ -0,0 +1,10 @@
+class Ll1:
+
+    def __init__(self, grammar):
+        self.gramamr = grammar
+
+    def first(S):
+        pass
+
+    def follow(S):
+        pass
--- lab6/main.py
+++ lab6/main.py
@@ -1,11 +1,8 @@
 from grammar import Grammar
+from ll1 import Ll1
 
 if __name__ == "__main__":
-    grammar = Grammar("g1.txt")
+    grammar = Grammar(input_file="g1.txt")
+    parser = Ll1(grammar=grammar)
 
-    print(f"File: {grammar.get_input_file()}")
-    print(f"Nonterminals: {grammar.get_nonterminals()}")
-    print(f"Terminals: {grammar.get_terminals()}")
-    print(f"Input: {grammar.get_input_symbol()}")
-    print(f"Production set: {grammar.get_productions()}")
-    print(f"CFG: {grammar.get_is_cfg()}")
+    
