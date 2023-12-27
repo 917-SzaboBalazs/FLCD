5138961552e7af819dd0f510cc7a07cb2069ef23
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-12-27 19:29:05 +0200

    Refactor LL1 parsing logic and update grammar rules

--- lab6/main.py
+++ lab6/main.py
@@ -7,12 +7,6 @@ def main():
     ll1 = Ll1(input_file=input_file)
     result = ll1.parse(input=input_string)
 
-    non_terminals = ll1.grammar().get_nonterminals()
-
-    print("Symbol\t\tFirst\t\tFollow")
-    for symbol in non_terminals:
-        print("{}\t\t{}\t\t{}".format(symbol, ll1.first(symbol), ll1.follow(symbol)))
-
     if result:
         print(f"Input string {input_string} is accepted by the grammar {input_file}")
     else:
