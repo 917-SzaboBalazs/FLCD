c03090471226ca682c0f1c29b4b1eb6f5cf6f56a
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-12-27 19:28:09 +0200

    Update LL1 parsing table generation

--- lab6/main.py
+++ lab6/main.py
@@ -2,10 +2,16 @@ from ll1 import Ll1
 
 def main():
     input_file = "g1.txt"
-    input_string = "abcc"
+    input_string = ""
 
     ll1 = Ll1(input_file=input_file)
     result = ll1.parse(input=input_string)
+
+    non_terminals = ll1.grammar().get_nonterminals()
+
+    print("Symbol\t\tFirst\t\tFollow")
+    for symbol in non_terminals:
+        print("{}\t\t{}\t\t{}".format(symbol, ll1.first(symbol), ll1.follow(symbol)))
 
     if result:
         print(f"Input string {input_string} is accepted by the grammar {input_file}")
