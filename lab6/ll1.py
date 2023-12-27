1cb06ced465544124a6f6384c33ec8ef13064831
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-12-27 15:50:31 +0200

    parsing table

--- lab6/ll1.py
+++ lab6/ll1.py
@@ -1,32 +1,69 @@
+from grammar import Grammar
+
 class Ll1:
 
-    def __init__(self, grammar):
-        self.gramamr = grammar
-        self.table = list()
+    def __init__(self, input_file):
+        self.__grammar = Grammar(input_file=input_file)
+        self.__first_col = dict()
+        self.__follow_col = dict()
+        self.__parsing_table = dict()
 
         self.__build_table()
 
-    def get_grammar(self):
-        return self.gramamr
+    def first(self, symbol):
+        return self.__first_col[symbol]
+    
+    def grammar(self):
+        return self.__grammar
 
-    def first(self, S):
-        return self.table[0][S]
+    def get_parsed_table(self):
+        return self.__parsing_table
+    
+    def parse(self, input):
+        # TODO: Implement parsing algorithm
+        pass
+    
+    def __check_ll1(self):
+        if not self.__grammar.check_cfg():
+            return False
 
-    def follow(self, S):
-        return self.table[1][S]
+        productions = self.__grammar.get_productions().get_all()
+
+        for rhs in productions.values():
+            no_non_terminals = 0
+            terminals = dict()
+
+            for rule in rhs:
+                if rule[0] in self.__grammar.get_nonterminals():
+                    no_non_terminals += 1
+
+                    if no_non_terminals > 1:
+                        return False
+                    
+                    elif no_non_terminals == 1 and len(rhs) > 1:
+                        return False
+                    
+                elif rule[0] in self.__grammar.get_terminals():
+                    terminals[rule[0]] = terminals.get(rule[0], 0) + 1
+
+                    if terminals[rule[0]] > 1:
+                        return False
+                    
+        return True
     
     def __build_table(self):
-        first_column = self.__build_first()
-        self.table.append(first_column)
+        if not self.__check_ll1():
+            raise ValueError("Grammar is not LL(1)")
 
-        follow_column = self.__build_follow()
-        self.table.append(follow_column)
+        self.__build_first()
+        self.__build_follow()
+        self.__build_parsing_table()
 
     def __build_first(self):
-        non_terminals = self.gramamr.get_nonterminals()
-        terminals = self.gramamr.get_terminals()
-        productions = self.gramamr.get_productions().get_all()
-        first_col = dict()
+        non_terminals = self.__grammar.get_nonterminals()
+        terminals = self.__grammar.get_terminals()
+        productions = self.__grammar.get_productions().get_all()
+        first_col = self.__first_col
 
         for symbol in non_terminals:
             first_col[symbol] = set()
@@ -34,27 +71,24 @@ class Ll1:
         for lhs, rhs in reversed(productions.items()):
             for rule in rhs:
                 if rule[0] in terminals:
-                    first_col[lhs[0]] = first_col[lhs[0]].union(rule[0])
+                    first_col[lhs[0]].add(str(rule[0]))
 
                 elif rule[0] in non_terminals:
-                    first_col[lhs[0]] = first_col[lhs[0]].union(first_col[rule[0]])
+                    first_col[lhs[0]].add(str(first_col[rule[0]]))
 
                 else:
                     raise ValueError("Invalid symbol in production rule: {}".format(rule[0]))
 
-        return first_col
-
     def __build_follow(self):
-        non_terminals = self.gramamr.get_nonterminals()
-        terminals = self.gramamr.get_terminals()
-        productions = self.gramamr.get_productions().get_all()
-        first_col = self.table[0]
-        follow_col = dict()
-
+        non_terminals = self.__grammar.get_nonterminals()
+        terminals = self.__grammar.get_terminals()
+        productions = self.__grammar.get_productions().get_all()
+        first_col = self.__first_col
+        follow_col = self.__follow_col
         for symbol in non_terminals:
             follow_col[symbol] = set()
 
-        follow_col[self.gramamr.get_input_symbol()] = set('$')
+        follow_col[self.__grammar.get_input_symbol()] = set('$')
 
         for key in productions.keys():
 
@@ -66,15 +100,38 @@ class Ll1:
                         if symbol == key[0]:
 
                             if i == len(rule) - 1:
-                                follow_col[symbol] = follow_col[symbol].union(follow_col[lhs[0]])
+                                follow_col[symbol].add(str(follow_col[lhs[0]]))
 
                             elif rule[i + 1] in terminals:
-                                follow_col[symbol] = follow_col[symbol].union([rule[i + 1]])
+                                follow_col[symbol].add(str(rule[i + 1]))
 
                             elif rule[i + 1] in non_terminals:
-                                follow_col[symbol] = follow_col[symbol].union(first_col[rule[i + 1]])
+                                follow_col[symbol].add(str(first_col[rule[i + 1]]))
                             
                             else:
                                 raise ValueError("Invalid symbol in production rule: {}".format(rule[i + 1]))
+                            
+    def __build_parsing_table(self):
+        non_terminals = self.__grammar.get_nonterminals()
+        terminals = self.__grammar.get_terminals()
+        productions = self.__grammar.get_productions().get_all()
 
-        return follow_col
+        for non_terminal in non_terminals:
+            self.__parsing_table[non_terminal] = dict()
+
+            for terminal in terminals:
+                self.__parsing_table[non_terminal][terminal] = None
+
+            self.__parsing_table[non_terminal]['$'] = None
+
+        for lhs, rhs in productions.items():
+            for rule in rhs:
+                if rule[0] in terminals:
+                    self.__parsing_table[lhs[0]][rule[0]] = rule
+
+                elif rule[0] in non_terminals:
+                    for terminal in self.__first_col[rule[0]]:
+                        self.__parsing_table[lhs[0]][terminal] = rule
+
+                else:
+                    raise ValueError("Invalid symbol in production rule: {}".format(rule[0]))
--- lab6/main.py
+++ lab6/main.py
@@ -1,20 +1,18 @@
-from grammar import Grammar
 from ll1 import Ll1
 
-def test(parser):
-    print(parser.get_grammar().get_productions())
+def main():
+    input_file = "g1.txt"
+    input_string = "abcc$"
 
-    print("{:<6} | {:<20} | {:<12}".format("Symbol", "First", "Follow"))  # Align the column headers
+    ll1 = Ll1(input_file=input_file)
+    result = ll1.parse(input=input_string)
 
-    for symbol in parser.get_grammar().get_nonterminals():
-        first = parser.first(symbol)
-        follow = parser.follow(symbol)
-        
-        print("{:<6} | {:<20} | {:<12}".format(symbol, str(first), str(follow)))  # Convert first and follow sets to strings before formatting
+    print(ll1.get_parsed_table())
 
+    if result:
+        print(f"Input string {input_string} is accepted by the grammar {input_file}")
+    else:
+        print(f"Input string {input_string} is not accepted by the grammar {input_file}")
 
 if __name__ == "__main__":
-    grammar = Grammar(input_file="g1.txt")
-    parser = Ll1(grammar=grammar)
-
-    test(parser)
+    main()
